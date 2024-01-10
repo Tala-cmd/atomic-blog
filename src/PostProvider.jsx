eb3563f7ba2c141fcce7923b71da7df392f5cf61
Tala <70854429+Tala-cmd@users.noreply.github.com>
2024-01-10 11:43:30 +0300

    Optimizing context re-renders

--- src/PostProvider.jsx
+++ src/PostProvider.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react"
+import { createContext, useContext, useMemo, useState } from "react"
 import { faker } from "@faker-js/faker";
 
 function createRandomPost() {
@@ -36,16 +36,19 @@ function PostProvider({ children }) {
     setPosts([]);
   }
 
+  const value = useMemo(()=>{
+    return {
+      posts: searchedPosts,
+      onAddPost: handleAddPost,
+      onClearPosts: handleClearPosts,
+      searchQuery,
+      setSearchQuery
+  }}, [searchQuery, searchedPosts])
+
   return (
     //2. PROVIDE VALUE TO CHILD COMPONENTS 
     <PostContext.Provider
-      value={{
-        posts: searchedPosts,
-        onAddPost: handleAddPost,
-        onClearPosts: handleClearPosts,
-        searchQuery,
-        setSearchQuery
-      }}> {children}
+      value={ value }> {children}
     </PostContext.Provider>
   )
 }
